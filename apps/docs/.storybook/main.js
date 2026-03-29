68662f1869589947eb84f3cfac9e17713536b762
Sandeep Jaiswar <Jaiswarsandeep119@gmail.com>
2026-03-29 21:45:20 +0530

    Refactor: code structure for improved readability and maintainability

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -1,3 +1,4 @@
+// This file has been automatically migrated to valid ESM format by Storybook.
 import { dirname, join } from "path";
 import { createRequire } from "module";
 import { fileURLToPath } from "url";
@@ -11,10 +12,12 @@ function getAbsolutePath(value) {
 
 const config = {
   stories: ["../stories/*.stories.tsx", "../stories/**/*.stories.tsx"],
+
   addons: [
     getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-docs")
   ],
+
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
@@ -34,11 +37,7 @@ const config = {
         },
       },
     };
-  },
-
-  docs: {
-    autodocs: true,
-  },
+  }
 };
 
 export default config;
--- apps/docs/eslint.config.js
+++ apps/docs/eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { config } from "@horcruxsys/nagini/eslint-config/react-internal.js";
 
 /** @type {import("eslint").Linter.Config[]} */
